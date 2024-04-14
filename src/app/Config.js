e602393dc81ef82dd9502fd30a0e9709381562e9
RAJA PINJA <raja_pinja@yahoo.com>
2024-04-14 06:12:43 +0530

    first commit

--- src/app/Config.js
+++ src/app/Config.js
@@ -2,7 +2,7 @@
 
 //export const BASE_URL = 'https://2025-2405-201-c032-b1d4-25c0-33ca-9016-dde1.ngrok-free.app';
 
-export const PROJECT_NAME = 'Training';
+export const PROJECT_NAME = 'Skilled Squad Central';
 
 export const BASE_URL = 'https://skillsquad-backend-app-4a40fef00f69.herokuapp.com';
 
