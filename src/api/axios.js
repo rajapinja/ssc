715799142507ddeae35b63c89aa21de03986695b
RAJA PINJA <raja_pinja@yahoo.com>
2024-04-14 06:19:28 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default axios.create({
-  // withCredentials: true,
-  baseURL: 'https://skillsquad-backend-app-4a40fef00f69.herokuapp.com',
-});
+// export default axios.create({
+//   // withCredentials: true,
+//   baseURL: 'https://skillsquad-backend-app-4a40fef00f69.herokuapp.com',
+// });
